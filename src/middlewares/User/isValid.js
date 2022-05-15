8688bd5e3040cbecf9bdd14254f21f2fca37043a
Daniel Batista <danielbped@gmail.com>
2022-05-15 13:51:14 -0300

    Feature: Username validation

--- src/middlewares/User/isValid.js
+++ src/middlewares/User/isValid.js
@@ -1,8 +1,9 @@
 const { StatusCodes } = require('http-status-codes');
 const errorMessages = require('../../utils/ErrorMessages');
 
+const MIN_SIZE = 4;
+
 const isNameValid = (req, res, next) => {
-  const MIN_SIZE = 4;
   const { firstName, lastName } = req.body;
   const fullName = firstName + lastName;
   const isNumberRegex = /\D/;
@@ -20,8 +21,19 @@ const isNameValid = (req, res, next) => {
   next();
 }
 
+const isUsernameValid = (req, res, next) => {
+  const { userName } = req.body;
+  
+  if (userName.lenght < MIN_SIZE) {
+    return res.status(StatusCodes.BAD_REQUEST).json({ message: errorMessages.invalidUsernameLength })
+  }
+
+  next()
+}
+
 const createUserValidation = (req, res, next) => {
-  isNameValid(req, res, next)
+  isNameValid(req, res, next);
+  isUsernameValid(req, res, next);
 };
 
 module.exports = {
--- src/utils/ErrorMessages.js
+++ src/utils/ErrorMessages.js
@@ -1,5 +1,6 @@
 module.exports = {
   taskNotFound: 'Task not found',
   invalidNameLength: '"firstName" and "lastName" length must be higher than 3',
-  invalidNameCharacter: '"firstName" and "lastName" cannot have special characters or numbers'
+  invalidNameCharacter: '"firstName" and "lastName" cannot have special characters or numbers',
+  invalidUsernameLength: '"Username" length must be higher than 3',
 }
