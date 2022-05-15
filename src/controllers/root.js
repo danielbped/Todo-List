1b6a98147c82991c5aad4299cfac32b23665d72b
Daniel Batista <danielbped@gmail.com>
2022-05-15 11:07:34 -0300

    Initial controllers w/ root + services

---
+++ src/controllers/root.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const user = require('./User');
+const task = require('./Task');
+const login = require('./Login');
+
+const root = express.Router({ mergeParams: true });
+
+user(root);
+task(root);
+login(root);
+
+module.exports = (app) => {
+  app.use('/', root)
+};
