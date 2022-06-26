501522b129c9ebcf6fc75788944a178b9ce88e3f
danielbped <danielbped@gmail.com>
2022-06-26 18:33:38 -0300

    Fix: Adds return value to login service

--- services/Login.js
+++ services/Login.js
@@ -1,21 +1,26 @@
 const { StatusCodes } = require('http-status-codes')
 const ErrorMessages = require('../utils/ErrorMessages')
-const { User } = require('./User')
+const { User } = require('../models')
 const bcrypt = require('bcrypt')
+const tokenGenerator = require('../middlewares/User/tokenGenerator')
 
 const login = async (req, res, next) => {
   try {
-    const { email, password, username } = req.body
+    const { email, password } = req.body
 
-    const user = User.findByEmail(email) || User.findByUsername(username)
+    const user = await User.findOne({ where: { email } })
 
     if (!user) {
       return res.status(StatusCodes.NOT_FOUND).json({ message: ErrorMessages.userNotFound })
     }
 
-    if (!bcrypt.compare(password, user.password)) {
+    if (!await bcrypt.compare(password, user.password)) {
       return res.status(StatusCodes.BAD_REQUEST).json({ message: ErrorMessages.wrongPassword })
     }
+
+    const token = await tokenGenerator(email, password)
+
+    return res.status(StatusCodes.OK).json({ token })
   } catch (err) {
     next(err)
   }
