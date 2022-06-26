f8e0694e838994bace8db23d1718c963cd91afa8
danielbped <danielbped@gmail.com>
2022-06-26 18:32:13 -0300

    Chore: Adds role field to user table

--- migrations/20220515153342-create-user.js
+++ migrations/20220515153342-create-user.js
@@ -28,6 +28,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
